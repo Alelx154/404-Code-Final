a920069b8bed2ea87865e4c2f9d072b00e676c15
Alelx154 <123492613+Alelx154@users.noreply.github.com>
2024-11-18 22:01:27 -0600

    Add files via upload

---
+++ Project-404-Final/Project-404/header.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <string>
+#include <iomanip>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+//Declerations of the functions
+int linearSearchFunction(vector<int> someVector, int value);
+
+int binarySearch(vector<int> someVector, int high, int low, int value);
